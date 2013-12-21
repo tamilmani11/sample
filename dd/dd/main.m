cb1f136fa5cf9b7f224d4a5e2f934555c304eabc
BSA Univ7 <bsauniv7@BSAs-iMac-3.local>
2013-12-21 17:21:59 +0530

    done

---
+++ dd/dd/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  dd
+//
+//  Created by BSA Univ7 on 21/12/13.
+//  Copyright (c) 2013 BSA Univ7. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
